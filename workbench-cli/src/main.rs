eff7f5d812a67809f3764a13f0e7e801f219312c
Aymeric Beaumet <hi@aymericbeaumet.com>
2023-04-04 15:02:45 +0200

    do not call functions that might exit

--- workbench-cli/src/main.rs
+++ workbench-cli/src/main.rs
@@ -8,7 +8,7 @@ struct Args {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let args = Args::parse();
+    let args = Args::try_parse()?;
 
     let (config, config_file) = workbench::Config::load(args.config_file).await?;
     let cwd = Path::parent(&config_file).expect("the config file must be in a directory");
