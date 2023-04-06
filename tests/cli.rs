cd9640614093f065e7cf0df831326be615ba4229
Aymeric Beaumet <hi@aymericbeaumet.com>
2023-04-06 18:59:50 +0200

    test: run tests in parallel

--- tests/cli.rs
+++ tests/cli.rs
@@ -7,47 +7,50 @@ const PATTERNS: [&str; 2] = [
     // try to match all workbench.toml files in the examples directory
     "examples/*/workbench.toml",
     // run the workbench.toml files in the tests directory
-    "tests/workbench*.toml",
+    "tests/*.toml",
 ];
 
 #[tokio::test(flavor = "multi_thread")]
 async fn test_invocations() {
-    let files: Vec<_> = PATTERNS
+    let root = env!("CARGO_MANIFEST_DIR");
+
+    let tasks = PATTERNS
         .iter()
-        .map(|pattern| format!("{}/{}", env!("CARGO_MANIFEST_DIR"), pattern))
+        .map(|pattern| format!("{}/{}", root, pattern))
         .flat_map(|pattern| glob(&pattern).unwrap().map(|entry| entry.unwrap()))
-        .collect();
+        .map(|file| {
+            // TODO: limit concurrency
+            tokio::spawn(async move {
+                let pretty_file = file.strip_prefix(root).unwrap();
 
-    assert!(!files.is_empty());
+                if read_file(&file, ".skip").await.is_some() {
+                    println!("[skipped] {:?}", &pretty_file);
+                    return;
+                }
 
-    // TODO: run in parallel with a concurrency of X
-    for file in files {
-        let skip = read_file(&file, ".skip").await;
-        if skip.is_some() {
-            println!("[skipped] {:?}", file);
-            continue;
-        }
+                let stdout = read_file(&file, ".stdout").await.unwrap_or_default();
+                let stderr = read_file(&file, ".stderr").await.unwrap_or_default();
 
-        let stdout = read_file(&file, ".stdout").await.unwrap_or_default();
-        let stderr = read_file(&file, ".stderr").await.unwrap_or_default();
+                // run and assert success
+                let assert = workbench(Some(&file)).success();
 
-        // run and assert success
-        let assert = workbench(Some(&file)).success();
+                // assert stdout
+                assert_str_eq!(
+                    std::str::from_utf8(&assert.get_output().stdout).unwrap(),
+                    stdout
+                );
 
-        // assert stdout
-        assert_str_eq!(
-            std::str::from_utf8(&assert.get_output().stdout).unwrap(),
-            stdout
-        );
+                // assert stderr
+                assert_str_eq!(
+                    std::str::from_utf8(&assert.get_output().stderr).unwrap(),
+                    stderr,
+                );
 
-        // assert stderr
-        assert_str_eq!(
-            std::str::from_utf8(&assert.get_output().stderr).unwrap(),
-            stderr,
-        );
+                println!("[ok]      {:?}", &pretty_file);
+            })
+        });
 
-        println!("[ok]      {:?}", file);
-    }
+    futures::future::join_all(tasks).await;
 }
 
 async fn read_file<P: AsRef<Path>>(filepath: P, suffix: &str) -> Option<String> {
