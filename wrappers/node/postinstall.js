b98ccfa3013ee566c330b8a5ade26efb06e796a3
Aymeric Beaumet <hi@aymericbeaumet.com>
2023-04-19 11:17:53 +0200

    wip

--- wrappers/node/postinstall.js
+++ wrappers/node/postinstall.js
@@ -7,44 +7,39 @@ const name = Object.keys(pkg.bin)[0];
 const dest = path.join(__dirname, pkg.bin[name]);
 
 const wrapper = new Wrapper(name, dest, [
-  // amd64
-  {
-    type: "Linux",
-    arch: "x64",
-    url: `${assetsPrefix}/run-x86_64-unknown-linux-gnu.tar.gz`,
-  },
-  {
-    type: "Freebsd",
-    arch: "x64",
-    url: `${assetsPrefix}/run-x86_64-unknown-freebsd.tar.gz`,
-  },
-  {
-    type: "Darwin",
-    arch: "x64",
-    url: `${assetsPrefix}/run-x86_64-apple-darwin.tar.gz`,
-  },
-  {
-    type: "Windows_NT",
-    arch: "x64",
-    url: `${assetsPrefix}/run-x86_64-pc-windows-msvc.zip`,
-    binSuffix: ".exe",
-  },
-  // arm64
+  // linux
   {
     type: "Linux",
     arch: "arm64",
     url: `${assetsPrefix}/run-aarch64-unknown-linux-gnu.tar.gz`,
   },
   {
-    type: "Darwin",
-    arch: "arm64",
-    url: `${assetsPrefix}/run-aarch64-apple-darwin.tar.gz`,
+    type: "Linux",
+    arch: "x64",
+    url: `${assetsPrefix}/run-x86_64-unknown-linux-gnu.tar.gz`,
   },
-  // i686
   {
     type: "Linux",
     arch: "ia32",
     url: `${assetsPrefix}/run-i686-unknown-linux-gnu.tar.gz`,
+  },
+  // macos
+  {
+    type: "Darwin",
+    arch: "arm64",
+    url: `${assetsPrefix}/run-aarch64-apple-darwin.tar.gz`,
+  },
+  {
+    type: "Darwin",
+    arch: "x64",
+    url: `${assetsPrefix}/run-x86_64-apple-darwin.tar.gz`,
+  },
+  // windows
+  {
+    type: "Windows_NT",
+    arch: "x64",
+    url: `${assetsPrefix}/run-x86_64-pc-windows-msvc.zip`,
+    binSuffix: ".exe",
   },
   {
     type: "Windows_NT",
