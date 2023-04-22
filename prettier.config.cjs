fe0116d69eaf4e7f579dd352f0b5c859f7c83771
Aymeric Beaumet <hi@aymericbeaumet.com>
2023-04-22 10:51:52 +0200

    wip

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -2,4 +2,12 @@ module.exports = {
   endOfLine: "lf",
   printWidth: 100,
   proseWrap: "always",
+  overrides: [
+    {
+      files: "*.yaml",
+      options: {
+        proseWrap: "preserve",
+      },
+    },
+  ],
 };
